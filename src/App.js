371672cb9eccc92f2e4f0f21bf50f0078acf5924
안자은 <wkiio@naver.com>
2019-05-25 15:27:19 +0900

    폴더명 변경 to  components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import {Header} from "./component/Header";
-import {Player} from "./component/Player";
+import {Header} from "./components/Header";
+import {Player} from "./components/Player";
 
 //React Element
 const players = [
